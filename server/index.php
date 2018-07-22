8c9410f08e06d8ec04e068baeffcb14be6875dcb
Robin Reumers <robin@leapwingaudio.com>
2018-07-22 23:01:13 +0200

    Wrote initial server for file/new and track/url

---
+++ server/index.php
@@ -0,0 +1,82 @@
+<?php
+require 'flight/Flight.php';
+require 'aws/aws-autoloader.php';
+
+////////////////////////////// Setup DB //////////////////////////////
+// Setup DB
+// register dB
+// Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=soundmarkerdev', 'root', 'mysql'));
+// $db = Flight::db();
+// $sql = "SELECT * FROM user WHERE email = '{$email}'";
+// $result = $db->query($sql);
+// if ($result != false) {
+//     return new user($result->fetch_assoc());
+// } else {
+//     return false;
+// }
+
+////////////////////////////// Setup S3 client //////////////////////////////
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$credentials = new Aws\Credentials\Credentials('AKIAJ2I2I4CLJHCP3NPQ', 'H31YdXr6TwoC8lRCSrFhA4n5JXZsiBIuqezP9P+b');
+$s3bucket = 'soundmarkersass-local-robin';
+
+$s3 = new Aws\S3\S3Client([
+    'version'     => 'latest',
+    'region'      => 'eu-west-1',
+    'credentials' => $credentials
+]);
+
+Flight::set("s3", $s3);
+Flight::set("s3bucket", $s3bucket);
+
+////////////////////////////// Routes - Global //////////////////////////////
+Flight::route('/', function(){
+    include 'soundmarker-development/dist/Soundmarker-sass/index.html';
+});
+
+////////////////////////////// Routes - /file/new //////////////////////////////
+Flight::route('POST /file/new', function() {
+$version_id = Flight::request()->data->version_id;
+$stream_identifier = Flight::request()->data->stream_identifier;
+$file_id = uniqid();
+
+// get the variables
+$s3 = Flight::get("s3");
+$s3bucket = Flight::get("s3bucket");
+
+try {
+    // Upload data.
+    $result = $s3->putObject([
+        'Bucket' => $s3bucket,
+        'Key'    => $file_id,
+        'Body'   => "Flight::request()->files[0]", // figuring out right way to get the file from the JSON
+        'ACL'    => 'public-read'
+    ]);
+	// Print the URL to the object.
+    echo $result['ObjectURL'] . PHP_EOL;
+} catch (S3Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
+// return ok
+Flight::json(array(
+   'file_id' => $file_id
+), 200);
+});
+
+
+////////////////////////////// Routes - /track/url //////////////////////////////
+Flight::route('GET /track/url', function() {
+$track_id = Flight::request()->data->track_id;
+
+// return ok
+Flight::json(array(
+   'track_url' => "https://d3k08uu3zdbsgq.cloudfront.net/Bruno-LetHerKnow.wav",
+   'track_hash' => "hash"
+), 200);
+});
+
+////////////////////////////// Start Flight //////////////////////////////
+Flight::start();
