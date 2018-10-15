67604ad91ec9a116b152ac6dedc177c6cd5dbb18
Robin Reumers <robin@leapwingaudio.com>
2018-10-15 15:59:35 +0200

    Fixed rogue identifier

--- server/index.php
+++ server/index.php
@@ -3,113 +3,113 @@ require 'flight/Flight.php';
 require 'vendor/autoload.php';
 
 // Unique ID
-class UUID {
-  public static function v3($namespace, $name) {
-    if(!self::is_valid($namespace)) return false;
+// class UUID {
+//   public static function v3($namespace, $name) {
+//     if(!self::is_valid($namespace)) return false;
 
-    // Get hexadecimal components of namespace
-    $nhex = str_replace(array('-','{','}'), '', $namespace);
+//     // Get hexadecimal components of namespace
+//     $nhex = str_replace(array('-','{','}'), '', $namespace);
 
-    // Binary Value
-    $nstr = '';
+//     // Binary Value
+//     $nstr = '';
 
-    // Convert Namespace UUID to bits
-    for($i = 0; $i < strlen($nhex); $i+=2) {
-      $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
-    }
+//     // Convert Namespace UUID to bits
+//     for($i = 0; $i < strlen($nhex); $i+=2) {
+//       $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+//     }
 
-    // Calculate hash value
-    $hash = md5($nstr . $name);
+//     // Calculate hash value
+//     $hash = md5($nstr . $name);
 
-    return sprintf('%08s-%04s-%04x-%04x-%12s',
+//     return sprintf('%08s-%04s-%04x-%04x-%12s',
 
-      // 32 bits for "time_low"
-      substr($hash, 0, 8),
+//       // 32 bits for "time_low"
+//       substr($hash, 0, 8),
 
-      // 16 bits for "time_mid"
-      substr($hash, 8, 4),
+//       // 16 bits for "time_mid"
+//       substr($hash, 8, 4),
 
-      // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 3
-      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+//       // 16 bits for "time_hi_and_version",
+//       // four most significant bits holds version number 3
+//       (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
 
-      // 16 bits, 8 bits for "clk_seq_hi_res",
-      // 8 bits for "clk_seq_low",
-      // two most significant bits holds zero and one for variant DCE1.1
-      (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+//       // 16 bits, 8 bits for "clk_seq_hi_res",
+//       // 8 bits for "clk_seq_low",
+//       // two most significant bits holds zero and one for variant DCE1.1
+//       (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
 
-      // 48 bits for "node"
-      substr($hash, 20, 12)
-    );
-  }
+//       // 48 bits for "node"
+//       substr($hash, 20, 12)
+//     );
+//   }
 
-  public static function v4() {
-    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+//   public static function v4() {
+//     return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
 
-      // 32 bits for "time_low"
-      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+//       // 32 bits for "time_low"
+//       mt_rand(0, 0xffff), mt_rand(0, 0xffff),
 
-      // 16 bits for "time_mid"
-      mt_rand(0, 0xffff),
+//       // 16 bits for "time_mid"
+//       mt_rand(0, 0xffff),
 
-      // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 4
-      mt_rand(0, 0x0fff) | 0x4000,
+//       // 16 bits for "time_hi_and_version",
+//       // four most significant bits holds version number 4
+//       mt_rand(0, 0x0fff) | 0x4000,
 
-      // 16 bits, 8 bits for "clk_seq_hi_res",
-      // 8 bits for "clk_seq_low",
-      // two most significant bits holds zero and one for variant DCE1.1
-      mt_rand(0, 0x3fff) | 0x8000,
+//       // 16 bits, 8 bits for "clk_seq_hi_res",
+//       // 8 bits for "clk_seq_low",
+//       // two most significant bits holds zero and one for variant DCE1.1
+//       mt_rand(0, 0x3fff) | 0x8000,
 
-      // 48 bits for "node"
-      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
-  }
+//       // 48 bits for "node"
+//       mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+//     );
+//   }
 
-  public static function v5($namespace, $name) {
-    if(!self::is_valid($namespace)) return false;
+//   public static function v5($namespace, $name) {
+//     if(!self::is_valid($namespace)) return false;
 
-    // Get hexadecimal components of namespace
-    $nhex = str_replace(array('-','{','}'), '', $namespace);
+//     // Get hexadecimal components of namespace
+//     $nhex = str_replace(array('-','{','}'), '', $namespace);
 
-    // Binary Value
-    $nstr = '';
+//     // Binary Value
+//     $nstr = '';
 
-    // Convert Namespace UUID to bits
-    for($i = 0; $i < strlen($nhex); $i+=2) {
-      $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
-    }
+//     // Convert Namespace UUID to bits
+//     for($i = 0; $i < strlen($nhex); $i+=2) {
+//       $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+//     }
 
-    // Calculate hash value
-    $hash = sha1($nstr . $name);
+//     // Calculate hash value
+//     $hash = sha1($nstr . $name);
 
-    return sprintf('%08s-%04s-%04x-%04x-%12s',
+//     return sprintf('%08s-%04s-%04x-%04x-%12s',
 
-      // 32 bits for "time_low"
-      substr($hash, 0, 8),
+//       // 32 bits for "time_low"
+//       substr($hash, 0, 8),
 
-      // 16 bits for "time_mid"
-      substr($hash, 8, 4),
+//       // 16 bits for "time_mid"
+//       substr($hash, 8, 4),
 
-      // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 5
-      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+//       // 16 bits for "time_hi_and_version",
+//       // four most significant bits holds version number 5
+//       (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
 
-      // 16 bits, 8 bits for "clk_seq_hi_res",
-      // 8 bits for "clk_seq_low",
-      // two most significant bits holds zero and one for variant DCE1.1
-      (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+//       // 16 bits, 8 bits for "clk_seq_hi_res",
+//       // 8 bits for "clk_seq_low",
+//       // two most significant bits holds zero and one for variant DCE1.1
+//       (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
 
-      // 48 bits for "node"
-      substr($hash, 20, 12)
-    );
-  }
+//       // 48 bits for "node"
+//       substr($hash, 20, 12)
+//     );
+//   }
 
-  public static function is_valid($uuid) {
-    return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
-                      '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
-  }
-}
+//   public static function is_valid($uuid) {
+//     return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+//                       '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
+//   }
+// }
 
 ////////////////////////////// Setup DB //////////////////////////////
 // Setup DB
@@ -162,13 +162,13 @@ $sql = "INSERT INTO Project (title, password, active) VALUES ('$project_title', 
 $result = $db->query($sql);
 $project_id = $db->lastInsertId();
 
-$v4uuid = UUID::v4();
-$sql = "UPDATE Project SET hash = '$v4uuid' WHERE project_id = '$project_id'";
+$uuid = uniqid('', true);
+$sql = "UPDATE Project SET hash = '$uuid' WHERE project_id = '$project_id'";
 $result = $db->query($sql);
 
 // return ok
 Flight::json(array(
-   'project_id' => $project_id, 'hash' => $v4uuid
+   'project_id' => $project_id
 ), 200);
 });
 
@@ -231,7 +231,7 @@ $metadata = (Flight::request()->data->metadata ?: "");
 $extension = (Flight::request()->data->extension ?: "");
 
 $db = Flight::db();
-$sql = "INSERT INTO File (version_id, file_name, file_size, metadata, extension, chunk_length, track_length, identifier) VALUES ('$version_id', '$file_name', '$file_size', '$extension', '$chunk_length', '$track_length', '$track_length')";
+$sql = "INSERT INTO File (version_id, file_name, file_size, metadata, extension, chunk_length, track_length, identifier) VALUES ('$version_id', '$file_name', '$file_size', '$extension', '$chunk_length', '$track_length', '$track_length' , '$identifier')";
 $result = $db->query($sql);
 $file_id = $db->lastInsertId();
 
