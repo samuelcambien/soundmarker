0a8b7d14e485a6f6bbc050cd6f79aae7d58e9466
Samuel <samuel@soundmarker.com>
2019-01-06 22:32:48 +0100

    move public introduction to public page

--- src/app/public-page/public-page.component.ts
+++ src/app/public-page/public-page.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Message} from "../message";
 import {RestCall} from "../rest/rest-call";
+import {PublicIntroductionComponent} from "./public-info/topics/public-introduction/public-introduction.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {TermsAcceptedServiceService} from "../terms-accepted-service.service";
 
 @Component({
   selector: 'app-public-page',
@@ -14,9 +17,16 @@ export class PublicPageComponent implements OnInit {
 
   ad;
 
-  constructor() { }
+  constructor(private modalService: NgbModal, private termsAcceptedService: TermsAcceptedServiceService) { }
+
+  openIntroduction() {
+    this.modalService.open(PublicIntroductionComponent, {size: "lg", backdrop: 'static', keyboard: false});
+  }
 
   ngOnInit() {
+    if (!this.termsAcceptedService.termsAccepted()) {
+      this.openIntroduction();
+    }
     RestCall.getAdId()
       .then(response => RestCall.getAd(response["ad_id"]))
       .then(response => this.ad = response);
