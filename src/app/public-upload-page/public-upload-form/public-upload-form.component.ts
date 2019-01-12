97ff2b56055c13fdd079b108fd24613b9ac867ab
Dieter Boels <dieterboels@Dieters-MacBook-Pro.local>
2019-01-12 19:34:55 +0100

    Upload form getallen in orde.

--- src/app/public-upload-page/public-upload-form/public-upload-form.component.ts
+++ src/app/public-upload-page/public-upload-form/public-upload-form.component.ts
@@ -25,8 +25,10 @@ export class PublicUploadFormComponent implements OnInit {
   sharemode: "email" | "link" = "email";
   expiration: "week" | "month" = "week";
   downloadable: boolean = false;
+  max_tracks: number = 30;
+  tracks_left = () => { return this.max_tracks - this.uploader.queue.length };
 
-  public validators = [Validators.required, Validators.pattern('^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$')];
+  validators = [Validators.required, Validators.pattern('^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$')];
 
   player;
 
@@ -34,6 +36,8 @@ export class PublicUploadFormComponent implements OnInit {
   @Output() uploading = new EventEmitter();
   @Output() finished = new EventEmitter();
   @Output() link = new EventEmitter<string>();
+
+
 
   @ViewChild('notes_element') notes_element: ElementRef;
 
