b1e04a54d4f0532da6ccb92eee24a9da71a83f8c
Samuel <samuel@soundmarker.com>
2019-01-26 09:03:21 -0800

    fix ad timings

--- src/app/public-page/public-page.component.ts
+++ src/app/public-page/public-page.component.ts
@@ -4,7 +4,7 @@ import {RestCall} from "../rest/rest-call";
 import {PublicIntroductionComponent} from "./public-info/topics/public-introduction/public-introduction.component";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {LocalStorageService} from "../local-storage.service";
-import {interval} from "rxjs";
+import {interval, Observable, timer} from "rxjs";
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {delay, map, tap} from "rxjs/operators";
 
@@ -32,8 +32,6 @@ import {delay, map, tap} from "rxjs/operators";
 })
 export class PublicPageComponent implements OnInit {
 
-  // Timing parameters
-  waitBeforeFirstAd = 2750; //ms
   exposureTime = 45;
 
   @Input() project_id;
@@ -62,53 +60,49 @@ export class PublicPageComponent implements OnInit {
     if (!this.localStorageService.termsAccepted()) {
       this.openIntroduction();
     }
-    this.getFirstAd();
-    interval(this.exposureTime * 1000)
-      .pipe(map(() => this.getNextAd()))
-      .pipe(tap(() => this.smaToggle = 0))
-      .pipe(delay(200))
-      .pipe(tap(() => this.showAd(this.smaHtml)))
-      .pipe(delay(200))
-      .subscribe(() => this.smaToggle = 1);
-  }
-
-  private getFirstAd() {
-    return this.getAd()
-      .then(response => {
-        this.smaHtml = response;
-          setTimeout(() => {
-            this.showAd(response);
-          }, this.waitBeforeFirstAd / 2)
-        }
-      ).then(() =>
-        setTimeout(() => {
-          this.smaToggle = 1;
-        }, this.waitBeforeFirstAd / 2)
+    this.loadFirstAd()
+      .then(() =>
+        timer(0, this.exposureTime * 1000)
+          .pipe(tap(() => this.smaToggle = 0))
+          .pipe(delay(200))
+          .pipe(tap(() => this.showAd()))
+          .pipe(delay(200))
+          .pipe(tap(() => this.smaToggle = 1))
+          .subscribe(() => this.loadNextAd())
       );
   }
 
-  private getNextAd(): Promise<any> {
-    return RestCall.getNextAdId(this.smaId, this.exposureTime)
-      .then(response =>
-        this.smaId = response["ad_id"]
-      ).then(() =>
-        RestCall.getAd(this.smaId)
-      ).then(response => {
-        if (response) this.smaHtml = response;
-      });
-  }
-
-  private getAd(): Promise<string> {
+  private loadFirstAd(): Promise<any> {
     return RestCall.getAdId()
+      .then(response => this.saveAd(response));
+  }
+
+  private loadNextAd(): Promise<any> {
+    return RestCall.getNextAdId(this.smaId, this.exposureTime)
+      .then(response => this.saveAd(response));
+  }
+
+  private saveAd(response): Promise<any> {
+    this.smaId = response["ad_id"];
+    return RestCall.getAd(this.smaId)
       .then(response => {
-        this.smaId = response["ad_id"];
-        return RestCall.getAd(this.smaId)
+        this.smaHtml = response;
       });
   }
 
-  private showAd(ad) {
-    this.sma.nativeElement.innerHTML = ad;
-    this.smaPhone.nativeElement.innerHTML = ad;
+  private changeAd(interval: Observable<number>) {
+    interval
+      .pipe(tap(() => this.smaToggle = 0))
+      .pipe(delay(200))
+      .pipe(tap(() => this.showAd()))
+      .pipe(delay(200))
+      .pipe(tap(() => this.smaToggle = 1))
+      .pipe(map(() => this.loadNextAd()))
+  }
+
+  private showAd() {
+    this.sma.nativeElement.innerHTML = this.smaHtml;
+    this.smaPhone.nativeElement.innerHTML = this.smaHtml;
   }
 
   private reset() {
