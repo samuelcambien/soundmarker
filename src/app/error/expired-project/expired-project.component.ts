700d334b62fa2b937d667275efa04c784d9fa2e1
Samuel <samuel@soundmarker.com>
2019-01-26 21:03:38 -0800

    expired-project componentje gebakken (niet verwarren met project-expired) ah nee sorry tis andersom

---
+++ src/app/error/expired-project/expired-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expired-project',
+  templateUrl: './expired-project.component.html',
+  styleUrls: ['./expired-project.component.scss']
+})
+export class ExpiredProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
