b613a657c5643c3246c0732712c7964f4828cabe
Robin Reumers <robinreumers@Robins-MacBook-Pro-2018.local>
2019-02-01 22:08:08 +0100

    Testing chinese characters 3

--- server/index.php
+++ server/index.php
@@ -985,7 +985,7 @@ $file_size = isset($getbody->file_size) ? $getbody->file_size : 0;
 $file_name = isset($getbody->file_name) ? $getbody->file_name : "";
 $metadata = isset($getbody->metadata) ? $getbody->metadata : "";
 $extension = isset($getbody->extension) ? $getbody->extension : "";
-$aws_path = $config['AWS_S3_PATH'].$version_id . "/" . $file_name;
+$aws_path = $config['AWS_S3_PATH'].$version_id . "/" . url_encode($file_name);
 
 // if user is able to upload file
 if (in_array($version_id, $_SESSION['user_versions'])) {
