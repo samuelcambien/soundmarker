e7009c95a927ec863d5bae2bdbd5212536941876
Dieter Boels <dieter@soundmarker.com>
2020-01-20 16:05:45 +0100

    Fix for iss#93

--- src/app/public-page/public-info/topics/demo/demo.component.ts
+++ src/app/public-page/public-info/topics/demo/demo.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, ViewChild} from '@angular/core';
 import {NgbActiveModal, NgbCarousel, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {BehaviorSubject, Observable, fromEvent, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-demo',
@@ -8,14 +9,20 @@ import {NgbActiveModal, NgbCarousel, NgbModal} from '@ng-bootstrap/ng-bootstrap'
 })
 export class DemoComponent implements OnInit {
 
+  @ViewChild('carousel') carousel: NgbCarousel;
   images = [`../assets/topics-img/sm-intro.webp`, `../assets/topics-img/intro-1.webp` , `../assets/topics-img/intro-2.webp`, `../assets/topics-img/intro-3.webp`, `../assets/topics-img/intro-4.webp`, `../assets/topics-img/intro-5.webp`];
+  images_loaded;
 
-  constructor(private modalService: NgbModal, private activeModal: NgbActiveModal) { }
-
-  ngOnInit() {
+  constructor(private modalService: NgbModal, private activeModal: NgbActiveModal) {
   }
 
-  @ViewChild('carousel') carousel: NgbCarousel;
+  ngOnInit() {
+    this.images_loaded = this.images.slice(0,3);
+    this.carousel.slide.subscribe(()=>{
+      if(this.images_loaded.length < this.images.length) this.images_loaded.push(this.images[this.images_loaded.length]);
+      else this.carousel.slide.complete();}
+      );
+  }
 
   close() {
     this.activeModal.close();
